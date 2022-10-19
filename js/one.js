fe4ad8ca9cec5ba518d24db6d1c211d629079373
denis5341884 <denis1021@mail.ru>
2022-10-20 00:10:16 +0600

    001020102022

--- js/one.js
+++ js/one.js
@@ -10,56 +10,6 @@ var tabledata = [
 ];
 
 
-//Create Date Editor
-var dateEditor = function(cell, onRendered, success, cancel){
-    //cell - the cell component for the editable cell
-    //onRendered - function to call when the editor has been rendered
-    //success - function to call to pass thesuccessfully updated value to Tabulator
-    //cancel - function to call to abort the edit and return to a normal cell
-
-    //create and style input
-    var cellValue = luxon.DateTime.fromFormat(cell.getValue(), "dd/MM/yyyy").toFormat("yyyy-MM-dd"),
-    input = document.createElement("input");
-
-    input.setAttribute("type", "date");
-
-    input.style.padding = "4px";
-    input.style.width = "100%";
-    input.style.boxSizing = "border-box";
-
-    input.value = cellValue;
-
-    onRendered(function(){
-        input.focus();
-        input.style.height = "100%";
-    });
-
-    function onChange(){
-        if(input.value != cellValue){
-            success(luxon.DateTime.fromFormat(input.value, "yyyy-MM-dd").toFormat("dd/MM/yyyy"));
-        }else{
-            cancel();
-        }
-    }
-
-    //submit new value on blur or change
-    input.addEventListener("blur", onChange);
-
-    //submit new value on enter
-    input.addEventListener("keydown", function(e){
-        if(e.keyCode == 13){
-            onChange();
-        }
-
-        if(e.keyCode == 27){
-            cancel();
-        }
-    });
-
-    return input;
-};
-
-
 //define row context menu contents
 var rowMenu = [
     {
@@ -92,8 +42,7 @@ var rowMenu = [
             },
         ]
     }
-]
-
+];
 //define column header menu as column visibility toggle
 var headerMenu = function(){
     var menu = [];
@@ -139,7 +88,6 @@ var headerMenu = function(){
 
    return menu;
 };
-
 //custom max min header filter
 var minMaxFilterEditor = function(cell, onRendered, success, cancel, editorParams){
 
@@ -193,7 +141,6 @@ var minMaxFilterEditor = function(cell, onRendered, success, cancel, editorParam
 
     return container;
  }
-
 //custom max min filter function
 function minMaxFilterFunction(headerValue, rowValue, rowData, filterParams){
     //headerValue - the value of the header filter element
@@ -217,7 +164,6 @@ function minMaxFilterFunction(headerValue, rowValue, rowData, filterParams){
 
     return true; //must return a boolean, true if it passes the filter.
 }
-
 
 var table = new Tabulator("#example-table", {
     height:"500px",
